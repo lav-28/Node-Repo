03706ee637ee4de8fd5d7022749c7d28e867bd28
lav.dhomne <lav.dhomne@nationalexpress.com>
2025-12-17 17:54:45 +0530

    till Section 2 : 13

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url')
 
-//-----------------------------------------------------FILE operation
+
+//-----------------------------------------------------
+// FILE operation
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -31,8 +34,27 @@ const http = require('http');
 //-----------------------------------------------------
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
+const dataObject = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
-    res.end('Hello from server!!');
+    console.log(req.url);
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is OVERVIEW');
+    }else if (pathName === '/product'){
+        res.end('This is PRODUCT');
+    }else if(pathName === '/api'){
+        res.writeHead(200, {'Content-type' : 'application/json'});
+        res.end(data);
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
